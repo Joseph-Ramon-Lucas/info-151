7a7f7a603fbb3b99962d32e2987e203649c61cdc
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2023-11-16 01:56:55 -0500

    finished responsive nav

---
+++ js/scripts-dist.js
@@ -0,0 +1,19 @@
+let mainNav = document.querySelector("#mainNav")
+let miniNav = document.querySelector("#miniNav")
+
+let burgerButton = document.getElementById("burger-container");
+
+let isNavVisible = false;
+
+
+burgerButton.addEventListener("click", () => {
+    if (!isNavVisible) {
+        miniNav .classList.add("showMiniNav");
+        isNavVisible = true;
+    } else {
+        miniNav.classList.remove("showMiniNav");
+        isNavVisible = false;
+    }
+    
+    console.log("button clickkkk");
+})
