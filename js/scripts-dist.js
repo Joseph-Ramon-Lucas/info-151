ea9e309dbab65e08873a1253a8bff66071cb62fe
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2023-11-27 15:37:20 -0500

    modals work with button functionality

--- js/scripts-dist.js
+++ js/scripts-dist.js
@@ -1,3 +1,5 @@
+
+// Showing navigation
 let mainNav = document.querySelector("#mainNav")
 let miniNav = document.querySelector("#miniNav")
 
@@ -17,3 +19,33 @@ burgerButton.addEventListener("click", () => {
     
     console.log("button clickkkk");
 })
+
+
+
+
+//showing popup signup form
+
+let signUpButtons = document.getElementsByClassName("signup");
+let signUpContainer = document.getElementById("signUp");
+
+let closeButton = document.getElementById("closeButton");
+let cancelButton = document.getElementById("cancel");
+
+
+for(let button=0; button < signUpButtons.length; button++) {
+    signUpButtons[button].addEventListener("click", () => {
+        signUpContainer.classList.add("showSignUpContainer");
+        
+    });
+}
+
+// console.log(closeButton);
+
+closeButton.addEventListener("click", () => {
+    signUpContainer.classList.remove("showSignUpContainer");
+})
+
+cancelButton.addEventListener("click", () => {
+    signUpContainer.classList.remove("showSignUpContainer");
+})
+
